d6220fa8af1203da202fa649a0c8c120f3b4e9dd
jonastb5055 <jonastbarchuk@gmail.com>
2026-04-07 12:14:50 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_tela04.xml

--- app/src/main/java/com/example/davi/Tela04.java
+++ app/src/main/java/com/example/davi/Tela04.java
@@ -13,12 +13,9 @@ public class Tela04 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela04);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
+
+
